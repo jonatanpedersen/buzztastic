aba07b541af1d8501914e7f14639c43585e8927c
Jonatan Pedersen <jp@jonatanpedersen.com>
2017-11-03 15:47:52 +0100

    fixes

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ async function main () {
 		}));
 
 		app.post('/api/buttons', callbackify(async (req, res) => {
-			const {buttonId} = v4();
+			const buttonId = v4();
 			const {name} = req.body;
 			const button = {buttonId, name, presses: []};
 
