796f1f2f27e4b126c71b0739c72f8d412494589c
Jonatan Pedersen <jp@jonatanpedersen.com>
2017-11-03 18:31:35 +0100

    rounds

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ async function main () {
 		const mongodbConnectionString = process.env.MONGODB_URI || 'mongodb://localhost/buzztastic';
 		const db = await MongoClient.connect(mongodbConnectionString);
 		const buttons = db.collection('buttons');
+		const rounds = db.collection('rounds');
 
 		const app = express();
 		app.use(express.static('public'));
@@ -22,6 +23,45 @@ async function main () {
 
 		app.get('/', callbackify(async (req, res) => {
 			res.render('index');
+		}));
+
+		app.get('/api/rounds/current', callbackify(async (req, res) => {
+			const round = await rounds.find({}).sort({timestamp: -1 }).limit(1).toArray().then(rounds => rounds.shift());
+
+			res.json(round);
+		}));
+
+		app.post('/api/rounds', callbackify(async (req, res) => {
+			const roundId = v4();
+			const timestamp = new Date();
+			const round = {roundId, timestamp, presses: []};
+
+			await rounds.insert(round);
+
+			io.emit('round-created', {roundId}, { for: 'everyone' });
+
+			res.json({roundId});
+		}));
+
+		app.post('/api/rounds/current/buttons/:buttonId/presses', callbackify(async (req, res) => {
+			const {buttonId} = req.params;
+			const timestamp = new Date();
+
+			const round = await rounds.find({}).sort({timestamp: -1 }).limit(1).toArray().then(rounds => rounds.shift());
+			const {roundId} = round;
+
+			await rounds.updateOne({roundId}, {
+				$push: {
+					presses: {
+						buttonId,
+						timestamp
+					}
+				}
+			});
+
+			io.emit('button-pressed', {buttonId, timestamp}, { for: 'everyone' });
+
+			res.status(204).end();
 		}));
 
 		app.get('/api/buttons', callbackify(async (req, res) => {
@@ -40,23 +80,6 @@ async function main () {
 			io.emit('button-added', {buttonId, name}, { for: 'everyone' });
 
 			res.json({buttonId, name});
-		}));
-
-		app.post('/api/buttons/:buttonId/presses', callbackify(async (req, res) => {
-			const {buttonId} = req.params;
-			const timestamp = new Date();
-
-			await buttons.updateOne({buttonId}, {
-				$push: {
-					presses: {
-						timestamp
-					}
-				}
-			});
-
-			io.emit('button-pressed', {buttonId, timestamp}, { for: 'everyone' });
-
-			res.status(204).end();
 		}));
 
 		app.use((err, req, res, next) => {
