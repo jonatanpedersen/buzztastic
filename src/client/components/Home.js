3beceb37b7610db9d084fb6295b2bb4abfcc623d
hilleer <hiller@live.dk>
2018-03-16 20:02:04 +0100

    Pusj latest

--- src/client/components/Home.js
+++ src/client/components/Home.js
@@ -7,13 +7,17 @@ export default () => {
 			<div class="home">
 				<h1 className="app-header">Buzztastic</h1>
 				<div className="home--container-create">
-					<input className="home--input" id="start-quiz"></input>
-					<button className=""><Link to="/start-quiz">Start quiz</Link></button>
+					<h2>Create new quiz</h2>
+					<label for="start-quiz">Please type in the name of your new quiz</label>
+					<input className="home--input" id="create-quiz"></input>
+					<button className="btn btn--blue"><Link to="/start-quiz">Start quiz</Link></button>
 				</div>
-				<hr />
+				<hr className="home--hr"/>
 				<div className="home--container-join">
+					<h2>Join existing quiz</h2>
+					<label for="join-quiz">Please type in the id of the quiz you want to join</label>
 					<input className="home--input" id="join-quiz"></input>
-					<button className=""><Link to="/start-quiz">Start quiz</Link></button>
+					<button className="btn btn--blue"><Link to="/start-quiz">Start quiz</Link></button>
 				</div>
 				{/* <button><Link to="/start-quiz">Home</Link></button> */}
 			</div>
---
+++ src/client/components/Quiz.js
@@ -0,0 +1,10 @@
+import React from 'react';
+export default () => {
+	return (
+		<section>
+			<div className="jumbotron">
+				<h1 className="display-3">quiz</h1>
+			</div>
+		</section>
+	);
+}
