2f26a345cd7df7e7a2788a702a0cf24813665be8
Jonatan Pedersen <jp@jonatanpedersen.com>
2018-03-16 14:48:25 +0100

    fixes build

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const webpack = require('webpack');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const {BundleAnalyzerPlugin} = require('webpack-bundle-analyzer');
 
 const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
 	template: './src/client/index.html',
@@ -16,30 +15,15 @@ module.exports = {
 		filename: 'index.js'
 	},
 	module: {
-		loaders: [
+		rules: [
 			{ test: /\.js?$/, loader: 'babel-loader', exclude: /node_modules/ }
 		]
 	},
 	devtool: 'hidden',
 	plugins: [
-		new BundleAnalyzerPlugin(),
 		HtmlWebpackPluginConfig,
 		new webpack.DefinePlugin({
 			'process.env.NODE_ENV': '"production"'
-		}),
-		new webpack.optimize.UglifyJsPlugin({
-			mangle: true,
-			compress: {
-				warnings: false, // Suppress uglification warnings
-				pure_getters: true,
-				unsafe: true,
-				unsafe_comps: true,
-				screw_ie8: true
-			},
-			output: {
-				comments: false,
-			},
-			exclude: [/\.min\.js$/gi] // skip pre-minified libs
 		}),
 		new webpack.DefinePlugin({
 			'process.env.NODE_ENV': '"production"'
